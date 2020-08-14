96c797debded8ce65dda8dcaa3752f06b939704b
ubinexy <ubinexy@gmail.com>
2020-08-15 01:26:38 +0800

    [shiqi][task1] feat: fetch products information from backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Products from './Products';
 
 class App extends Component {
   state = {
     products: undefined,
-    // products: [{ id: 1, name: '可乐1', price: '1元', unit: '瓶' }],
   };
 
   componentDidMount() {
@@ -14,33 +13,37 @@ class App extends Component {
   }
 
   getData() {
-    let url = 'localhost:8080/api/products';
-    fetch(url)
+    let url = 'http://0.0.0.0:8080/api/products';
+
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
       .then((results) => results.json())
-      .then((results) => this.setState({ products: results }));
+      .then((results) => {
+        this.setState({ products: results });
+      });
   }
 
   render() {
     return (
       <main className="app">
-          <Router>
+        <Router>
           <header>
             <nav className="selected">
-              <Link to="/shop">商城</Link>
+              <Link to="/">商城</Link>
             </nav>
             <nav>
-              <Link to="/shop">订单</Link>
+              <Link to="/order">订单</Link>
             </nav>
             <nav>
-              <Link to="/shop">+ 添加商品</Link>
+              <Link to="/addProduct">+ 添加商品</Link>
             </nav>
           </header>
-            <Route
-              path="/shop"
-              render={() => <Products products={this.state.products} />}
-            ></Route>
-          </Router>
-
+        </Router>
+        <Products products={this.state.products} />
       </main>
     );
   }
--- src/Products.js
+++ src/Products.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Product.scss';
 import defaultImage from './assets/product_image_placeholder.png';
 
-function Products(props) {
-  console.log(props);
-  return (<div className="products">
-    {
-      props.products.map(item=> 
-        (<div key={item.name} className="product">
-          <img
-            src={defaultImage}
-            className="product-image"
-            alt="product-image"
-          />
-          <p className="product-name">{item.name}</p>
-          <p className="prodcut-price">
-            单价:{item.price}/{item.unit}
-          </p>
-          <div className="add-product">
-            <button>+</button>
-          </div>
-        </div>
-        )
-      )
+class Products extends Component {
+  render() {
+    if (this.props.products === undefined) {
+      return <div className="products"></div>;
     }
-    </div>
-  );
+    return (
+      <div className="products">
+        {this.props.products.map((item) => (
+          <div key={item.name} className="product">
+            <img
+              src={defaultImage}
+              className="product-image"
+              alt="product-image"
+            />
+            <p className="product-name">{item.name}</p>
+            <p className="prodcut-price">
+              单价:{item.price}/{item.unit}
+            </p>
+            <div className="add-product">
+              <button>+</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Products;
